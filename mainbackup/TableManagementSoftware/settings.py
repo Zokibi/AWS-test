ce7b3527a6d228a85876f727145a9db937998171
zok! <112133076+Zokibi@users.noreply.github.com>
2024-12-04 16:48:23 -0500

    update settings.py

--- mainbackup/TableManagementSoftware/settings.py
+++ mainbackup/TableManagementSoftware/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ebr9epyr&uoxsjg2p$)!mf=!qj_(q&50x2460xd^^mwyuu%p-b'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ["18.212.97.150"]
+ALLOWED_HOSTS = []
 
 
 # Application definition
